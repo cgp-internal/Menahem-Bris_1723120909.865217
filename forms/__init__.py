8081e4b9ef41749fb47ca0a1908afacc52ccd154
cgp-client <code.gen.pro.system@gmail.com>
2024-08-08 16:16:10 +0300

    Update forms/__init__.py

--- forms/__init__.py
+++ forms/__init__.py
@@ -1,4 +1,6 @@
 from .expense_form import ExpenseForm
 from .income_form import IncomeForm
+from .login_form import LoginForm
+from .register_form import RegisterForm
 
-__all__ = ['ExpenseForm', 'IncomeForm']
+__all__ = ['ExpenseForm', 'IncomeForm', 'LoginForm', 'RegisterForm']
